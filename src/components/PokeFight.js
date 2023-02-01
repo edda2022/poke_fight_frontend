f2c5cd2ad0b2512a0833aa29e1e9107849baeddf
cele <cele@cele.com>
2023-02-01 18:00:21 +0100

    players

--- src/components/PokeFight.js
+++ src/components/PokeFight.js
@@ -1,28 +1,38 @@
-import { useState, useEffect } from "react"
-import { useParams, useNavigate } from "react-router-dom";
-import axios from 'axios'
-import { Link } from "react-router-dom";
-import PokemonList from "./PokemonList";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+export default function PokeFight({ playerA, playerB }) {
+  const [firstPokemon, setFirstPokemon] = useState("");
+  const [secondPokemon, setSecondPokemon] = useState("");
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8082/pokemon/${playerA}`)
+      .then((response) => {
+        setFirstPokemon(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-export default function PokeFight({playerA, playerB }) {
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8082/pokemon/${playerB}`)
+      .then((response) => {
+        setSecondPokemon(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-
-    return(
-        <>
-        <div className="pokefight">
-             
-             <div> 1 player selected: {playerA}
-             </div>
-
-             <div>
-                2 player selected: {playerB}
-
-              
-             </div>
-
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="pokefight">
+        <div> 1 player selected: {firstPokemon.name}</div>
+        <div>2 player selected: {secondPokemon.name}</div>
+      </div>
+    </>
+  );
 }
