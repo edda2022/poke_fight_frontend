6aa5d32476d561e5748360bccac3005087aabcfe
edda2022 <candyreinecke@googlemail.com>
2023-02-01 18:01:01 +0100

    poke fight styleing first steps

--- src/components/PokeFight.js
+++ src/components/PokeFight.js
@@ -1,28 +1,28 @@
-import { useState, useEffect } from "react"
-import { useParams, useNavigate } from "react-router-dom";
-import axios from 'axios'
-import { Link } from "react-router-dom";
-import PokemonList from "./PokemonList";
-
-
-
-export default function PokeFight({playerA, playerB }) {
-
-
-    return(
-        <>
-        <div className="pokefight">
-             
-             <div> 1 player selected: {playerA}
-             </div>
-
-             <div>
-                2 player selected: {playerB}
-
-              
-             </div>
-
+export default function PokeFight({ capitalizeFirstLetter, playerA, playerB }) {
+  return (
+    <>
+    <div className="d-flex flex-column justify-content-center align-items-center">
+    <h2 className="justify-content-center align-items-center">Here are the 2 Pokémons fighting against each other</h2>
         </div>
-        </>
-    )
+      <div className="pokefight">
+        <div className="card playerA">
+          <img src="" className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <h5 className="card-title">{capitalizeFirstLetter(playerA)}</h5>
+            <p className="card-text">Here comes the Pokemon info</p>
+          </div>
+        </div>
+
+        <div className="card playerB">
+          <img src="" className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <h5 className="card-title">{capitalizeFirstLetter(playerB)}</h5>
+            <p className="card-text">Here comes the Pokemon info</p>
+          </div>
+        </div>
+      </div>
+      <button type="button" class="btn btn-danger btn-lg">FIGHT!</button>
+
+    </>
+  );
 }
