1b0ab069723354d30e0e460937f7a674d0bcc1bf
cele <cele@cele.com>
2023-02-02 22:22:12 +0100

    add-responsive-searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,26 @@
-
-
- export default function Searchbar({handleOnChange, inputSearch, handleSearch}) {
-
-    return(
-        <>
-        <form onSubmit={handleSearch} className="d-flex bg-light searchform">
-            <input className="form-control me-2" type="text" required placeholder="Search for a Pokemon name or ID" onChange={handleOnChange} value={inputSearch}></input>
-            <input type="submit" value="Search" className="btn btn-secondary btn-outline-warning"></input>
-        </form>
-        </>
-    )
+export default function Searchbar({
+  handleOnChange,
+  inputSearch,
+  handleSearch,
+}) {
+  return (
+    <div>
+      <form onSubmit={handleSearch} className="search-bar">
+        <input
+          className="form-control bg-light"
+          type="text"
+          required
+          placeholder="Search for a Pokemon name or ID"
+          onChange={handleOnChange}
+          value={inputSearch}
+        ></input>
+        <button
+          type="submit"
+          className="btn btn-secondary btn-outline-warning "
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
 }
